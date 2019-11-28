c41035b9fd34afa77e7c89de242d57db5309a514
Paul Ruggiero <paul.ruggiero@vidyard.com>
2019-11-28 18:11:09 -0500

    hungry

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const randomMovement = (possibleMovements, snake, food) => {
     movement = 'up';
   }
 
-  if (food.length > 0 && snake.health < 30) {
+  if (food.length > 0 && snake.health < 120) {
     if (food[0].y !== location[0].y) {
       if (food[0].y < location[0].y && possibleMovements.indexOf('up') !== -1) {
         movement = 'up';
