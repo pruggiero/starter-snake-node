de4cd048b351f191f3339f74a0d7f7bf9b16de64
Paul Ruggiero <paul.ruggiero@vidyard.com>
2019-11-28 13:24:08 -0500

    remove other snakes

--- index.js
+++ index.js
@@ -96,22 +96,22 @@ const possibleMovements = (snakebody, othersnakes) => {
     }
   });
 
-  othersnakes.forEach(snake => {
-    snake.body.forEach(cordinate => {
-      if (snakebody[0].x === cordinate.x + 1) {
-        possibleMovements = removePossibleMovement(possibleMovements, 'left');
-      }
-      if (snakebody[0].x === cordinate.x - 1) {
-        possibleMovements = removePossibleMovement(possibleMovements, 'right');
-      }
-      if (snakebody[0].y === cordinate.y + 1) {
-        possibleMovements = removePossibleMovement(possibleMovements, 'up');
-      }
-      if (snakebody[0].y === cordinate.y - 1) {
-        possibleMovements = removePossibleMovement(possibleMovements, 'down');
-      }
-    });
-  });
+  // othersnakes.forEach(snake => {
+  //   snake.body.forEach(cordinate => {
+  //     if (snakebody[0].x === cordinate.x + 1) {
+  //       possibleMovements = removePossibleMovement(possibleMovements, 'left');
+  //     }
+  //     if (snakebody[0].x === cordinate.x - 1) {
+  //       possibleMovements = removePossibleMovement(possibleMovements, 'right');
+  //     }
+  //     if (snakebody[0].y === cordinate.y + 1) {
+  //       possibleMovements = removePossibleMovement(possibleMovements, 'up');
+  //     }
+  //     if (snakebody[0].y === cordinate.y - 1) {
+  //       possibleMovements = removePossibleMovement(possibleMovements, 'down');
+  //     }
+  //   });
+  // });
 
   // if (possibleMovements === []) {
   //   possibleMovements['up'];
