dfa9c108fa35938b28e2f22e6efc1f3df1afbf1f
Paul Ruggiero <paul.ruggiero@vidyard.com>
2019-11-29 09:21:15 -0500

    hungry at the start

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const randomMovement = (possibleMovements, snake, food) => {
   // }
 
 
-  if (food.length > 0 && ((snake.health < 60 && snake.body.length <= 9) || snake.health < 30) ) {
+  if (food.length > 0 && ((snake.health < 80 && snake.body.length <= 9) || snake.health < 30) ) {
     var nearbyFood = food[0];//getNearbyFood(food, location);
     if (nearbyFood.y !== location[0].y) {
       if (nearbyFood.y < location[0].y && possibleMovements.indexOf('up') !== -1) {
