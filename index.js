5112d907d24f0eec5e437f51b12ff79f71dc34af
Paul Ruggiero <paul.ruggiero@vidyard.com>
2019-11-28 20:21:03 -0500

    basics

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const randomMovement = (possibleMovements, snake, food) => {
 
 
   if (food.length > 0 && snake.health < 60) {
-    nearbyFood = getNearbyFood(food, location);
+    nearbyFood = foods[0];//getNearbyFood(food, location);
     if (nearbyFood.y !== location[0].y) {
       if (nearbyFood.y < location[0].y && possibleMovements.indexOf('up') !== -1) {
         movement = 'up';
