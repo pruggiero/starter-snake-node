36e47190d6436c8d179631eebdef0eebd3533f4a
Paul Ruggiero <paul.ruggiero@vidyard.com>
2019-11-28 21:15:23 -0500

    eat more often

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const randomMovement = (possibleMovements, snake, food) => {
   }
 
 
-  if (food.length > 0 && snake.health < 30) {
+  if (food.length > 0 && snake.health < 40) {
     var nearbyFood = food[0];//getNearbyFood(food, location);
     if (nearbyFood.y !== location[0].y) {
       if (nearbyFood.y < location[0].y && possibleMovements.indexOf('up') !== -1) {
